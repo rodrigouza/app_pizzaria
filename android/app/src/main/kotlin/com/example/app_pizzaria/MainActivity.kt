9d67b0447ad3b39957dc8287ce4ecb609c6f16ee
Rodrigo Elias de Souza <92738606+rodrigouza@users.noreply.github.com>
2022-05-23 12:56:32 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/app_pizzaria/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_pizzaria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types
+
+import 'package:app_pizzaria/tela_home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Pizza_App());
