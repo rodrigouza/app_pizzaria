2c378e5ef25b4525f0a97067f5e00f93380c3419
Rodrigo Elias de Souza <92738606+rodrigouza@users.noreply.github.com>
2022-05-25 23:35:54 -0300

    Ajuste de formatação

--- lib/app/pizza_app.dart
+++ lib/app/pizza_app.dart
@@ -1,22 +1,31 @@
-// ignore_for_file: prefer_const_constructors
+
 
 import 'package:flutter/material.dart';
 
 class Pizza_App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     //retorna ao main() toda funcionalidade estrutural/grafica do material design
     return MaterialApp(
+
         //Scaffold -> cria a plataforma/andaime onde o app será construido
         home: Scaffold(
             appBar: AppBar(
-                backgroundColor: Colors.red, title: Text('Pizza Planet')),
+              backgroundColor: Colors.red, title: Text('Pizza Planet')),
+            
             body: Column(children: [
               Container(
-                  width: double.infinity,
-                  height: 275,
-                  alignment: Alignment.topCenter,
-                  child: Stack(children: []))
-            ])));
+                width: double.infinity,
+                height: 275,
+                alignment: Alignment.topCenter,
+                child: Stack(
+                  children: [
+                  ]
+                )
+              )
+            ])
+         )
+       );
   }
 }
