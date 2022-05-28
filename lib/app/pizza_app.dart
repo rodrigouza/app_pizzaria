728ae3fa8bfba9f8e5741ee71ba01d93e59da818
Rodrigo Elias de Souza <92738606+rodrigouza@users.noreply.github.com>
2022-05-28 11:44:44 -0300

    Update pizza_app.dart

--- lib/app/pizza_app.dart
+++ lib/app/pizza_app.dart
@@ -1,31 +1,84 @@
-
-
 import 'package:flutter/material.dart';
 
 class Pizza_App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     //retorna ao main() toda funcionalidade estrutural/grafica do material design
     return MaterialApp(
+      //Scaffold -> cria a plataforma/andaime onde o app será construido
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('Pizza Planet')),
 
-        //Scaffold -> cria a plataforma/andaime onde o app será construido
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.red, title: Text('Pizza Planet')),
-            
-            body: Column(children: [
-              Container(
-                width: double.infinity,
-                height: 275,
-                alignment: Alignment.topCenter,
-                child: Stack(
-                  children: [
-                  ]
-                )
-              )
-            ])
-         )
-       );
+          body: Column(children: [
+            Container(
+              width: double.infinity,
+              height: 275,
+              alignment: Alignment.topCenter,
+              child: Stack(children: [
+                Container(
+                  alignment: Alignment.topCenter,
+                  child: Image.network('https://pizzariadesucesso.com/wp-content/uploads/2018/05/pepperoni-pizza.jpg')),
+
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      width: double.infinity,
+                      height: 80,
+                      color: Color.fromARGB(130, 0, 0, 0),
+                      child: Column(
+                        children:[
+                          Text('Quem prova não esquece', textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
+
+                          Text('Pepperoni',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.red, fontSize: 30.0, fontWeight: FontWeight.bold))
+                        ]
+                      )
+                    )),
+                ])),
+            Container(
+              height: 320,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+
+                    ElevatedButton.icon(onPressed: () {},
+                    label: Text('Delivery',
+                    style: TextStyle(fontSize: 36.0, color: Colors.black, fontWeight: FontWeight.bold)),
+                    icon: Icon(Icons.delivery_dining, size: 70.0, color: Colors.black45),
+                    style: ButtonStyle(
+                    alignment: Alignment.centerLeft,
+                    backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.redAccent),
+                    minimumSize: MaterialStateProperty.all(Size(390,90))
+                    )),
+                    
+                    ElevatedButton.icon(onPressed: () {},
+                    label: Text('Fidelidade',
+                    style: TextStyle(fontSize: 36.0, color: Colors.black, fontWeight: FontWeight.bold)),
+                    icon: Icon(Icons.heart_broken, size: 70.0, color: Colors.black45),
+                    style: ButtonStyle(
+                    alignment: Alignment.centerLeft,
+                    backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.lightBlueAccent),
+                    minimumSize: MaterialStateProperty.all(Size(390,90))
+                    )),
+
+                    ElevatedButton.icon(onPressed: () {},
+                    label: Text('Cardápio',
+                    style: TextStyle(fontSize: 36.0, color: Colors.black, fontWeight: FontWeight.bold)),
+                    icon: Icon(Icons.menu_book, size: 70.0, color: Colors.black45),
+                    style: ButtonStyle(
+                    alignment: Alignment.centerLeft,
+                    backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.lightGreenAccent),
+                    minimumSize: MaterialStateProperty.all(Size(390,90))
+                    ))
+
+              ]),
+            )    
+          ])
+      )
+    );
   }
 }
